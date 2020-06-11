e4abcb6346c68fcebabfd9a1cd2fa0c090ca226d
DanielLorenzo <61579849+DanielLorenzo@users.noreply.github.com>
2020-06-11 13:45:40 +0200

    Update BoardTest.java

--- src/test/java/es/codeurjc/ais/tictactoe/BoardTest.java
+++ src/test/java/es/codeurjc/ais/tictactoe/BoardTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class BoardTest {
+public class BoardTest {
 private Board board;
 	
 	@BeforeEach
